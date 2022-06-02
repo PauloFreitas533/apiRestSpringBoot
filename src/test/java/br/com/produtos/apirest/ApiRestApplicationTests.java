45e5bbfdf8b0a4294e18c66cf12270d004c9d087
Paulo Freitas <paulinho.f533@gmail.com>
2022-06-01 21:30:55 -0300

    First commit

---
+++ src/test/java/br/com/produtos/apirest/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.produtos.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
